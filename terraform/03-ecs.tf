ddc115aac8e52c6df5facbf9ed47400e876758ee
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2024-08-23 18:46:48 +0300

    change image

--- terraform/03-ecs.tf
+++ terraform/03-ecs.tf
@@ -127,7 +127,7 @@ resource "aws_ecs_task_definition" "app" {
     "essential": true,
     "portMappings": [
       {
-        "containerPort": 80,
+        "containerPort": 8080,
         "hostPort": 80,
         "protocol": "tcp"
       }
@@ -167,7 +167,7 @@ resource "aws_ecs_service" "app" {
   load_balancer {
     target_group_arn = aws_lb_target_group.app.arn
     container_name   = "my-app"
-    container_port   = 80
+    container_port   = 8080
   }
 
   tags = {
