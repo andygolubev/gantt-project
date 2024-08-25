f83aab9821a2cbbdf8050e3effc0b0bdb4718026
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2024-08-25 09:40:06 +0300

    remove aurora

--- terraform/04-rds.tf
+++ terraform/04-rds.tf
@@ -88,101 +88,101 @@ resource "aws_iam_role_policy_attachment" "rds_monitoring_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
 }
 
-# Create an RDS Aurora Serverless Cluster for PostgreSQL
-resource "aws_rds_cluster" "aurora_postgres" {
-  cluster_identifier      = "aurora-postgres-cluster"
-  engine                  = "aurora-postgresql"
-  engine_mode             = "provisioned"
-  engine_version          = "16.3"
-  database_name           = "gantt"
-  master_username         = "auroraadmin"
-  master_password         = "aurorapassword"
-  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
-  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+# # Create an RDS Aurora Serverless Cluster for PostgreSQL
+# resource "aws_rds_cluster" "aurora_postgres" {
+#   cluster_identifier      = "aurora-postgres-cluster"
+#   engine                  = "aurora-postgresql"
+#   engine_mode             = "provisioned"
+#   engine_version          = "16.3"
+#   database_name           = "gantt"
+#   master_username         = "auroraadmin"
+#   master_password         = "aurorapassword"
+#   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+#   db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
 
-  serverlessv2_scaling_configuration {
-    max_capacity = 1.0
-    min_capacity = 0.5
-  }
+#   serverlessv2_scaling_configuration {
+#     max_capacity = 1.0
+#     min_capacity = 0.5
+#   }
 
-  skip_final_snapshot     = true
-  deletion_protection     = false
+#   skip_final_snapshot     = true
+#   deletion_protection     = false
 
-  tags = {
-    Name = "aurora-postgres-cluster"
-  }
-}
+#   tags = {
+#     Name = "aurora-postgres-cluster"
+#   }
+# }
 
-resource "aws_rds_cluster_instance" "aurora_instance" {
-  cluster_identifier = aws_rds_cluster.aurora_postgres.id
-  instance_class     = "db.serverless"
-  engine             = aws_rds_cluster.aurora_postgres.engine
-  engine_version     = aws_rds_cluster.aurora_postgres.engine_version
-}
+# resource "aws_rds_cluster_instance" "aurora_instance" {
+#   cluster_identifier = aws_rds_cluster.aurora_postgres.id
+#   instance_class     = "db.serverless"
+#   engine             = aws_rds_cluster.aurora_postgres.engine
+#   engine_version     = aws_rds_cluster.aurora_postgres.engine_version
+# }
 
-# Create an IAM Role for RDS Proxy
-resource "aws_iam_role" "rds_proxy_role" {
-  name = "rds-proxy-role"
+# # Create an IAM Role for RDS Proxy
+# resource "aws_iam_role" "rds_proxy_role" {
+#   name = "rds-proxy-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "rds.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "rds.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
+# }
 
-# Attach the correct policy for RDS Proxy to the IAM Role
-resource "aws_iam_role_policy_attachment" "rds_proxy_role_policy" {
-  role       = aws_iam_role.rds_proxy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
-}
+# # Attach the correct policy for RDS Proxy to the IAM Role
+# resource "aws_iam_role_policy_attachment" "rds_proxy_role_policy" {
+#   role       = aws_iam_role.rds_proxy_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+# }
 
-# Create an RDS Proxy for Aurora PostgreSQL
-resource "aws_db_proxy" "aurora_proxy" {
-  name                   = "aurora-postgres-proxy"
-  engine_family          = "POSTGRESQL"
-  require_tls            = true
-  idle_client_timeout    = 1800
-  debug_logging          = false
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  vpc_subnet_ids         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+# # Create an RDS Proxy for Aurora PostgreSQL
+# resource "aws_db_proxy" "aurora_proxy" {
+#   name                   = "aurora-postgres-proxy"
+#   engine_family          = "POSTGRESQL"
+#   require_tls            = true
+#   idle_client_timeout    = 1800
+#   debug_logging          = false
+#   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+#   vpc_subnet_ids         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
 
-  role_arn               = aws_iam_role.rds_proxy_role.arn  # Specify the role ARN
+#   role_arn               = aws_iam_role.rds_proxy_role.arn  # Specify the role ARN
 
-  auth {
-    auth_scheme = "SECRETS"
-    description = "Aurora RDS Proxy"
-    iam_auth    = "DISABLED"
-    secret_arn  = aws_secretsmanager_secret.aurora_secret.arn
-  }
+#   auth {
+#     auth_scheme = "SECRETS"
+#     description = "Aurora RDS Proxy"
+#     iam_auth    = "DISABLED"
+#     secret_arn  = aws_secretsmanager_secret.aurora_secret.arn
+#   }
 
-  tags = {
-    Name = "aurora-postgres-proxy"
-  }
-}
+#   tags = {
+#     Name = "aurora-postgres-proxy"
+#   }
+# }
 
-# Create Secrets Manager Secret for RDS Proxy
-resource "aws_secretsmanager_secret" "aurora_secret" {
-  name = "aurora-secret"
+# # Create Secrets Manager Secret for RDS Proxy
+# resource "aws_secretsmanager_secret" "aurora_secret" {
+#   name = "aurora-secret"
 
-  tags = {
-    Name = "aurora-secret"
-  }
-}
+#   tags = {
+#     Name = "aurora-secret"
+#   }
+# }
 
-# Store the secret value in Secrets Manager
-resource "aws_secretsmanager_secret_version" "aurora_secret_value" {
-  secret_id     = aws_secretsmanager_secret.aurora_secret.id
-  secret_string = jsonencode({
-    username = "auroraadmin"
-    password = "aurorapassword"
-  })
-}
+# # Store the secret value in Secrets Manager
+# resource "aws_secretsmanager_secret_version" "aurora_secret_value" {
+#   secret_id     = aws_secretsmanager_secret.aurora_secret.id
+#   secret_string = jsonencode({
+#     username = "auroraadmin"
+#     password = "aurorapassword"
+#   })
+# }
 
